948f2a7ed74b56d8e10104a109b0f54fe5a2d2ba
Alpamys Qanybet <alpamys.qanybet@gmail.com>
2017-09-24 23:32:30 +0500

    some changes to output properly

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 // skip this line if using Create React Native App
 // AppRegistry.registerComponent('HelloworldApp', () => HelloworldApp);
+
+/*
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
